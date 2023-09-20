7b69ce8b2b52d5495db36f3ed145707fb27b975e
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-09-21 02:33:37 +0300

    run detached

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 cd /www/grafana/
 COMPOSE_PROJECT_NAME=gratheon docker-compose down
 
-COMPOSE_PROJECT_NAME=gratheon docker-compose up --build
+COMPOSE_PROJECT_NAME=gratheon docker-compose up --build -d
